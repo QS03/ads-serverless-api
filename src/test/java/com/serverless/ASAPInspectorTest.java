fdc54dce992acacfb463d1acfc93351744e59b31
pycoach <zoltan@tecoachs.com>
2020-06-03 15:24:40 -0400

    ASAP Inspector api implemented

---
+++ src/test/java/com/serverless/ASAPInspectorTest.java
@@ -0,0 +1,27 @@
+package com.serverless;
+
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+
+public class ASAPInspectorTest {
+    @Test
+    /*
+     *  Valid start date and end date
+     * */
+    public void ValidCase() {
+        ASAPInspectorHandler handler = new ASAPInspectorHandler();
+
+        Map<String, Object> input = new HashMap<String, Object>();
+        Map<String, String> queryStringParameters = new HashMap<String, String>() ;
+        input.put("queryStringParameters", queryStringParameters);
+
+        ApiGatewayResponse response = handler.handleRequest(input, null);
+        System.out.println(response.getBody());
+
+        assertEquals(response.getStatusCode(), 200);
+    }
+}
